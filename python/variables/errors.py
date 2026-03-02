ab4b5ddba2f388c6fc97f15a55ebc4a0120efdd9
Diogo-Serra <di-code@outlook.com>
2026-03-02 22:35:03 +0000

    updates

---
+++ python/variables/errors.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+butterflies = 10
+beetles = 12
+ladybugs = 20
+
+print(f"I caught {butterflies} 🦋 butterflies!")
+print(f'I caught {beetles} 🪲 beetles!')
+print(f'I caught {ladybugs} 🐞 ladybugs!')
+print(f'I caught {butterflies + beetles + ladybugs} total bugs!')
