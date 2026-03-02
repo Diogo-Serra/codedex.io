d7a4c8b6481a4c43b9b64eed0fdc36b137edb535
Diogo-Serra <di-code@outlook.com>
2026-03-02 22:59:46 +0000

    updates

---
+++ python/control_flow/ph_levels.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+def validate_ph(ph):
+    if ph < 0 or ph > 7:
+        print("Invalid ph")
+        exit()
+
+
+def ph_levels():
+    ph = int(input("Enter ph(0 - 14)"))
+    validate_ph(ph)
+    if ph > 7:
+        print("Basic")
+    elif ph < 7:
+        print("Acidic")
+    else:
+        print("Neutral")
+
+
+ph_levels()
