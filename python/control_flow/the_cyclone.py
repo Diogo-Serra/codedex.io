c7d9f605f4245458086cedd28fd94d3e11018185
Diogo-Serra <di-code@outlook.com>
2026-03-02 23:49:02 +0000

    updates

--- python/control_flow/the_cyclone.py
+++ python/control_flow/the_cyclone.py
@@ -4,10 +4,10 @@
 height = int(input("Your height: "))
 credits = int(input("Your credits: "))
 if height >= 137 and credits >= 10:
-  print("Enjoy the ride!")
+    print("Enjoy the ride!")
 elif credits < 10:
-  print("You don't have enough credits.")
+    print("You don't have enough credits.")
 elif height < 137:
-  print("You are not tall enough")
+    print("You are not tall enough")
 else:
-  print("Requirements not met")
+    print("Requirements not met")
