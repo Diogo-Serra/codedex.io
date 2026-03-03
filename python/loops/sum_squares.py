84f55fff4ec1ed1cbd2531f9b47d07e3f0c895f3
Diogo-Serra <di-code@outlook.com>
2026-03-03 19:46:38 +0000

    updates

--- python/loops/sum_squares.py
+++ python/loops/sum_squares.py
@@ -1,2 +1,21 @@
 #!/usr/bin/env python3
+"""
+A number is "squared" when it is either multiplied by itself
+or taken to the second power (e.g., 4² = 4 x 4 = 16).
 
+First, ask the user for an integer with int(input()) and
+store it in a number variable. Then, define a total
+variable with an initial value of 0.
+
+Note: You can pass a string prompt to int(input()).
+
+Next, use a for loop and range() function to calculate the total of the
+squares of all integers from 1 to that number.
+
+Last, print the output as an integer value.
+"""
+number = int(input("Choose a number: "))
+total = 0
+for i in range(1, number + 1):
+    total += number * number
+print(total)
