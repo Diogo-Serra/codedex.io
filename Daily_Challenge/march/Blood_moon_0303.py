827f65f4b4884c6b1ace7f8a83e542fa64526786
Diogo-Serra <di-code@outlook.com>
2026-03-03 15:54:26 +0000

    updates

--- Daily_Challenge/march/Blood_moon_0303.py
+++ Daily_Challenge/march/Blood_moon_0303.py
@@ -5,12 +5,9 @@ import datetime
 # 2 hours and 48 minutes apart
 def blood_moon(time):
     now = datetime.datetime.strptime(time, "%H:%M")
-    later = now + datetime.timedelta(hours=2, minutes=48)
-    print(f'[\"{later.strftime("%H:%M")}\",', end=" ")
-    later += datetime.timedelta(hours=2, minutes=48)
-    print(f'\"{later.strftime("%H:%M")}\",', end=" ")
-    later += datetime.timedelta(hours=2, minutes=48)
-    print(f'\"{later.strftime("%H:%M")}\"]')
-
-
-blood_moon("10:32")
+    my_timestamps = []
+    my_timestamps.append(now + datetime.timedelta(hours=2, minutes=48))
+    my_timestamps.append(datetime.timedelta(hours=2, minutes=48))
+    my_timestamps.append(datetime.timedelta(hours=2, minutes=48))
+    for time in my_timestamps:
+        print(time)
