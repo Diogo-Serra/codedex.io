55d9e7df6fa67bc8fd761639ef4065e3344ccac4
Diogo-Serra <di-code@outlook.com>
2026-03-03 20:51:45 +0000

    updates

---
+++ python/lists/grocery.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+grocery = [
+    '🥚 Eggs',
+    '🥑 Avocados',
+    '🍪 Cookies',
+    '🌶 Hot Pepper Jam',
+    '🫐 Blueberries',
+    '🥦 Broccoli']
+for i in grocery:
+    print(i)
