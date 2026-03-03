831cab1f70aca6c757a9aaea5708335cc4911864
Diogo-Serra <di-code@outlook.com>
2026-03-03 18:17:58 +0000

    updates

--- python/control_flow/seasons_year.py
+++ python/control_flow/seasons_year.py
@@ -2,10 +2,12 @@
 
 month = int(input("Enter the month(1 - 12): "))
 if month == 1 or month == 2 or month == 3:
-    print("Winter")
-if month == 4 or month == 5 or month == 6:
-    print("Spring")
-if month == 7 or month == 8 or month == 9:
-    print("Summer")
-if month == 10 or month == 11 or month == 12:
-    print("Autumn")
+    print("Winter 🌨️")
+elif month == 4 or month == 5 or month == 6:
+    print("Spring 🌱")
+elif month == 7 or month == 8 or month == 9:
+    print("Summer 🌻")
+elif month == 10 or month == 11 or month == 12:
+    print("Autumn 🍂")
+else:
+    print("Invalid")
