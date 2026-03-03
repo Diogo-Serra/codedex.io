10ad531025bfbe31a2aedb4c6b7ce45ea8fc1e5d
Diogo-Serra <di-code@outlook.com>
2026-03-03 18:15:29 +0000

    updates

---
+++ python/control_flow/seasons_year.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+month = int(input("Enter the month(1 - 12): "))
+if month == 1 or month == 2 or month == 3:
+    print("Winter")
+if month == 4 or month == 5 or month == 6:
+    print("Spring")
+if month == 7 or month == 8 or month == 9:
+    print("Summer")
+if month == 10 or month == 11 or month == 12:
+    print("Autumn")
