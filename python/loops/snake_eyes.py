7c49c40a04a5f9aa1aa973911189e4bb1ec0396c
Diogo-Serra <di-code@outlook.com>
2026-03-03 18:55:10 +0000

    updates

--- python/loops/snake_eyes.py
+++ python/loops/snake_eyes.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 import random
 
-dye1 = 0
-dye2 = 0
-while dye1 != 1:
+dye1 = random.randint(1, 6)
+dye2 = random.randint(1, 6)
+total = dye1 + dye2
+while total != 2:
     dye1 = random.randint(1, 6)
+    dye2 = random.randint(1, 6)
+    total = dye1 + dye2
     print("Nope")
-    while dye2 != 1:
-        dye2 = random.randint(1, 6)
-        print("Nope")
 print("Snake eyes!")
