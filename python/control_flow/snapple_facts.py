58f213cd39ca1c8949d594ba0b25a7c9b3554bea
Diogo-Serra <di-code@outlook.com>
2026-03-03 18:03:54 +0000

    updates

---
+++ python/control_flow/snapple_facts.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import random
+
+i = random.randint(1, 6)
+if i == 1:
+    print("Flamingos turn pink by eating shrimp.")
+elif i == 2:
+    print("Honey never goes bad.")
+elif i == 3:
+    print("Shrimp can only swim backwards")
+elif i == 4:
+    print("A taste bud\'s life is about 10 days.")
+elif i == 5:
+    print("You can\'t sneeze while sleeping.")
+elif i == 6:
+    print("Tiny pocket in jeans was for watches.")
