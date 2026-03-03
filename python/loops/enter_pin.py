6e15007e72ed2e943e034513bdf6744bf5acc564
Diogo-Serra <di-code@outlook.com>
2026-03-03 16:18:40 +0000

    updates

---
+++ python/loops/enter_pin.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+# Write code below 💖
+print('BANK OF CODEDEX')
+
+pin = int(input("Enter your PIN: "))
+
+while pin != 1234:
+    pin = int(input("Incorrect PIN. Enter your PIN again: "))
+
+if pin == 1234:
+    print("PIN accepted")
