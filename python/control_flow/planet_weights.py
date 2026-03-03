b5d9fafdce9992759643ba809637bf732b50086b
Diogo-Serra <di-code@outlook.com>
2026-03-03 18:27:25 +0000

    updates

--- python/control_flow/planet_weights.py
+++ python/control_flow/planet_weights.py
@@ -1,29 +1,5 @@
 #!/usr/bin/env python3
-"""
-The year is 2199... Humanity is now an interplanetary species,
-freely traveling across the solar system! 🚀
 
-Create a weight conversion program that:
-
-Asks the user for their Earth weight (as a float).
-Asks the user for a planet number (as an int).
-Then, use an if/elif/else statement to calculate
-the user's weight on the destination planet.
-
-To calculate the user's weight:
-
-destination weight=Earth weight × relative gravity
-Number	Planet	Relative Gravity
-1	Mercury	0.38
-2	Venus	0.91
-3	Mars	0.38
-4	Jupiter	2.53
-5	Saturn	1.07
-6	Uranus	0.89
-7	Neptune	1.14
-If the user enters a planet number outside of 1 - 7,
-print a message that says 'Invalid number'
-"""
 earth_weight = float(input("Your Earth weight: "))
 planet_number = int(input("Planet number: "))
 destination_weight = 0.0
