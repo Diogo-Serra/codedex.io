f9b5790301c1e5bcb64f4552b85f8dd927e94973
Diogo-Serra <di-code@outlook.com>
2026-03-04 18:50:54 +0000

    updates

---
+++ python/functions/fortune_cookie_bonus.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+class Phrases:
+    def __init__(self, index, phrase):
+        self.index = index
+        self.phrase = phrase
+
+    def get_phrase(my_phrases, index) -> str:
+        for phrase in my_phrases:
+            if phrase.index == index:
+                return phrase.phrase
+
+
+my_phrases = [
+    Phrases(0, "Don't pursue happiness - create it."),
+    Phrases(1, "All things are difficult before they are easy."),
+    Phrases(2, "The early bird gets the worm, but the second mouse"
+               " gets the cheese."),
+    Phrases(3, "Someone in your life needs a letter from you."),
+    Phrases(4, "Don't just think. Act!"),
+    Phrases(5, "Your heart will skip a beat."),
+    Phrases(6, "The fortune you search for is in another cookie."),
+    Phrases(7, "Help! I'm being held prisoner in a Chinese bakery!")]
+
+
+test = Phrases.get_phrase(my_phrases, 0)
+print(test)
