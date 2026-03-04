27b97ff3a2682bc57fb8968a9103e721269f7913
Diogo-Serra <di-code@outlook.com>
2026-03-04 15:08:27 +0000

    updates

---
+++ python/lists/inventory.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+lego_parts = [
+    8980, 7323, 5343, 82700, 92232, 1203, 7319, 8903, 2328, 1279,
+    679, 589]
+minimum = min(lego_parts)
+maximum = max(lego_parts)
+print(f"Min: {minimum} - Max: {maximum}")
