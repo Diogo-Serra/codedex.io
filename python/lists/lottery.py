7f2d1d0f320204c1626c671d4160bc8842647284
Diogo-Serra <di-code@outlook.com>
2026-03-04 17:43:35 +0000

    updates

---
+++ python/lists/lottery.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+from random import randint
+
+my_numbers = []
+winning_numbers = []
+for i in range(0, 5):
+    my_numbers.append(randint(1, 26))
+    winning_numbers.append(randint(1, 26))
+my_numbers.append(randint(1, 26))
+winning_numbers.append(randint(1, 26))
+print(my_numbers)
+print(winning_numbers)
