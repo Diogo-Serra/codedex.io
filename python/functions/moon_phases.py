ea1fd407c9b5ff7fc563a3cb302c20e7699a5dd4
Diogo-Serra <di-code@outlook.com>
2026-03-04 21:14:12 +0000

    updates

--- python/functions/moon_phases.py
+++ python/functions/moon_phases.py
@@ -1,29 +1,20 @@
 #!/usr/bin/env python3
 
-class MoonPhases():
-    def __init__(self, phrase, emoji):
-        self.phrase = phrase
-        self.emoji = emoji
-
-    def get_emoji(phases_moon, phrase):
-        for phase in phases_moon:
-            if phase.phrase == phrase:
-                print(phase.emoji)
-
-
-def moon_phase(phases_moon, phrase):
-    MoonPhases.get_emoji(phases_moon, phrase)
-
-
 phases_moon = [
-    MoonPhases("New Moon", "🌑"),
-    MoonPhases("Waxing Crescent", "🌒"),
-    MoonPhases("First Quarter", "🌓"),
-    MoonPhases("Waxing Gibbous", "🌔"),
-    MoonPhases("Full Moon", "🌕"),
-    MoonPhases("Waning Gibbous", "🌖"),
-    MoonPhases("Last Quarter", "🌗"),
-    MoonPhases("Waning Crescent", "🌘")]
+    ("New Moon", "🌑"),
+    ("Waxing Crescent", "🌒"),
+    ("First Quarter", "🌓"),
+    ("Waxing Gibbous", "🌔"),
+    ("Full Moon", "🌕"),
+    ("Waning Gibbous", "🌖"),
+    ("Last Quarter", "🌗"),
+    ("Waning Crescent", "🌘")]
 
 
-moon_phase(phases_moon, "New Moon")
+def moon_phase(phase):
+    for name, emoji in phases_moon:
+        if phase == name:
+            print(emoji)
+
+
+moon_phase("New Moon")
