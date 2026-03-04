843e4562d18473e0e45768392a53da7640ebd47d
Diogo-Serra <di-code@outlook.com>
2026-03-04 18:24:36 +0000

    updates

---
+++ python/functions/fortune_cookie.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+import random
+
+list = [
+    "Don't pursue happiness – create it.",
+    "All things are difficult before they are easy.",
+    "The early bird gets the worm, but the second mouse gets the cheese.",
+    "Someone in your life needs a letter from you.",
+    "Don't just think. Act!",
+    "Your heart will skip a beat.",
+    "The fortune you search for is in another cookie.",
+    "Help! I'm being held prisoner in a Chinese bakery!"]
+
+
+def fortune():
+    f = random.randint(0, 7)
+    print(f)
+
+
+fortune()
