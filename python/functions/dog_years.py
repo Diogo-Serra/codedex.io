b3dc2fe91c6cd7a82d3ef30c0e81a6aa07cbf13e
Diogo-Serra <di-code@outlook.com>
2026-03-04 21:38:08 +0000

    updates

--- python/functions/dog_years.py
+++ python/functions/dog_years.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
 def dog_years(name, age):
-    age += age * 7
-    print(f"{name} is {age} years old in human years.")
+    age = age * 7
+    return (f"{name} is {age} years old in human years.")
 
 
 dog_years("Kovu", 6)
