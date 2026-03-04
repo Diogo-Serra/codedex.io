ae8f5623f4a5de80ab562ef17b9e5754477c7495
Diogo-Serra <di-code@outlook.com>
2026-03-04 16:44:11 +0000

    updates

---
+++ python/lists/mixtape.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+playlist = [
+  'Porches - rangerover',
+  'Mount Eerie - You Swan, Go On',
+  'Carolyn Polachek - Look at Me Now',
+  'Pinegrove - Darkness',
+  'LVL UP - Spirit Was',
+  'Mitski - First Love / Late Spring'
+]
+for song in playlist:
+    print(song)
+for song in range(len(playlist)):
+    print(playlist[song])
