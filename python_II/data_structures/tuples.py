59897659ef81e3ebd64ca44528c024c7e20e2ba6
Diogo-Serra <di-code@outlook.com>
2026-03-05 16:06:58 +0000

    updates

---
+++ python_II/data_structures/tuples.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+Lisbon = ("Lisbon", 38.722252, -9.139337)
+Paris = ("Paris", 48.856613, 2.352222)
+London = ("London", 51.507351, -0.127758)
+print(Lisbon)
+print(Paris)
+print(London)
