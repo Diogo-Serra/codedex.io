0a5173eec80890a2d31e122c1df22e6b70434677
Diogo-Serra <di-code@outlook.com>
2026-03-05 14:09:23 +0000

    updates

--- python/modules/slot_machine.py
+++ python/modules/slot_machine.py
@@ -1,7 +1,22 @@
 #!/usr/bin/env python3
 import random
 
-symbols = ['🍒', ' 🍇', '🍉', '7️']
-
+symbols = ['🍒', ' 🍇', '🍉', '7️⃣']
 result = random.choices(symbols, k=3)
 print(f"{result[0]} | {result[1]} | {result[2]}")
+if result[0] == result[1] == result[2] == '7️⃣':
+    print("Jackpot! 💰")
+
+
+def play():
+    again = input("Play again?(Y/N): ")
+    while again == 'Y':
+        result = random.choices(symbols, k=3)
+        print(f"{result[0]} | {result[1]} | {result[2]}")
+        if result[0] == result[1] == result[2] == '7️⃣':
+            print("Jackpot! 💰")
+        again = input("Play again?(Y/N): ")
+    exit()
+
+
+play()
