4b1d8da06f115017792fcf162b23ada6d0ec9a48
Diogo-Serra <di-code@outlook.com>
2026-03-05 14:45:21 +0000

    updates

---
+++ python/modules/bday/main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import datetime
+import bday_messages
+
+today = datetime.date.today()
+next_birthday = datetime.date(2026, 7, 22)
+days_away = next_birthday - today
+if today == next_birthday:
+    print(bday_messages.random_message)
+else:
+    print(f"My next birthday is {days_away.days} days away")
