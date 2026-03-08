1e13860a6ebc8a347960e896f3a2e691ffb67d3f
Diogo-Serra <di-code@outlook.com>
2026-03-08 18:14:52 +0000

    updates

--- Daily_Challenge/International_Womens_Day.py
+++ Daily_Challenge/International_Womens_Day.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
 def analyze(percentages):
-    net_change_per_year = round((percentages[-1] - percentages[0]) / (len(percentages) - 1), 4)
+    net_change_per_year = round((percentages[-1] - percentages[0])
+                                / (len(percentages) - 1), 4)
 
     first_avg = sum(percentages[:3]) / 3
     last_avg = sum(percentages[-3:]) / 3
@@ -12,7 +13,8 @@ def analyze(percentages):
     else:
         trend = "declining"
 
-    dips = sum(1 for i in range(1, len(percentages)) if percentages[i] < percentages[i - 1])
+    dips = sum(1 for i in range(1, len(percentages))
+               if percentages[i] < percentages[i - 1])
 
     return net_change_per_year, trend, dips
 
@@ -21,7 +23,9 @@ def analyze(percentages):
 print(analyze([31.0, 31.0, 33.0, 35.0, 36.0, 36.0, 36.2, 36.7, 37.1]))
 
 # Amazon (2014-2024)
-print(analyze([42.0, 43.0, 42.0, 43.0, 44.0, 44.0, 44.6, 44.8, 44.7, 45.0, 45.8]))
+print(analyze([42.0, 43.0, 42.0, 43.0, 44.0,
+               44.0, 44.6, 44.8, 44.7, 45.0, 45.8]))
 
 # Apple (2014-2024)
-print(analyze([30.0, 31.0, 32.0, 32.0, 33.0, 34.0, 34.0, 34.8, 35.0, 35.0, 35.3]))
+print(analyze([30.0, 31.0, 32.0, 32.0, 33.0, 34.0, 34.0, 34.8, 35.0,
+               35.0, 35.3]))
