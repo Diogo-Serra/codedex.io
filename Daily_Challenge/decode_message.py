14c4c09d6c94c97230935c70f36bcb0f3939340e
Diogo-Serra <di-code@outlook.com>
2026-03-15 17:27:49 +0000

    updates

---
+++ Daily_Challenge/decode_message.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+def decode_message(message: str, shift: int) -> str:
+    decoded = ""
+    shift = shift % 26
+    for ch in message:
+        if ch == " ":
+            decoded += " "
+        else:
+            decoded += chr((ord(ch) - ord("a") - shift) % 26 + ord("a"))
+    return decoded
+
+
+decode_message
