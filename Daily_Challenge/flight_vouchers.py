39106118eed54f541cca8d4fa3efe5a8770633fa
Diogo-Serra <di-code@outlook.com>
2026-03-18 15:56:03 +0000

    updates

--- Daily_Challenge/flight_vouchers.py
+++ Daily_Challenge/flight_vouchers.py
@@ -1,17 +1,23 @@
 #!/usr/bin/env python3
 
 def pick_voucher(vouchers: list, delays: list, max_wait: int) -> str:
-    for voucher in vouchers:
-        print(voucher)
-        print(delays)
-        print(max_wait)
+    best_value = 0
+    index = -1
+    for i, (voucher, delay) in enumerate(zip(vouchers, delays)):
+        print(f"Index: {i} Price: {voucher} Delay: {delay}")
+        if delay < max_wait:
+            if index == -1 or best_value < (voucher / delay):
+                best_value = voucher / delay
+                index = i
+    return index
 
 
 def tester() -> None:
-    vouchers: list = [100, 200, 300, 400]
-    delays: list = [1, 2, 3, 4]
-    max_wait: int = 2
-    pick_voucher(vouchers, delays, max_wait)
+    vouchers = [50, 120, 20]
+    delays = [2, 4, 1]
+    max_wait = 3
+    index = pick_voucher(vouchers, delays, max_wait)
+    print(index)
 
 
 tester()
